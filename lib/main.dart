ba25d5babf635560d0ae91a5a9b9f03b51bc7d05
akilino <amendez@nakitec.com>
2019-12-03 17:29:15 +0000

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import "package:flare_flutter/flare_cache_builder.dart";
 import 'package:flare_flutter/flare_controller.dart';
 import "package:flutter/material.dart";
 import 'package:flare_dart/math/mat2d.dart';
+import 'package:flare_dart/math/vec2d.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
@@ -40,7 +41,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with FlareController {
   double _rockAmount = 1;
   //double _speed = 0.00001157407; // 24 HOUR
-  double _speed = 0.05;
+  double _speed = 1;
   double _rockTime = 0;
   double _cloudyTime = 0;
   bool _isPaused = false;
@@ -49,6 +50,14 @@ class _MyHomePageState extends State<MyHomePage> with FlareController {
   bool end = false;
   String _timeString;
   double counter = 0;
+  ActorNode _nodeglobal;
+  List<String> moonState = [
+    'New Moon',
+    'Full Moon',
+    'First Quarter',
+    'Third Quarter'
+  ];
+  Vec2D _lunanewpos = Vec2D();
 
   @override
   void initState() {
@@ -60,6 +69,40 @@ class _MyHomePageState extends State<MyHomePage> with FlareController {
 
   @override
   void initialize(FlutterActorArtboard artboard) {
+    _nodeglobal = artboard.getNode("Global");
+    ActorNode nodeluna =
+        _nodeglobal.children.firstWhere((node) => node.name == 'Sol y Luna');
+    ActorNode nodelunaLuz =
+        nodeluna.children.firstWhere((node) => node.name == 'Luna luz');
+    ActorNode luna =
+        nodeluna.children.firstWhere((node) => node.name == 'Luna');
+    FlutterActorEllipse estadolunaluz =
+        nodelunaLuz.children.firstWhere((node) => node.name == 'Ellipse Path');
+    FlutterActorShape estadoluna =
+        luna.children.firstWhere((node) => node.name == 'estadoluna');
+    final _random = new Random();
+    String randomluna = moonState[_random.nextInt(moonState.length)];
+    switch ('New Moon') {
+      case 'New Moon':
+        print('New Moon');
+        estadoluna.x = estadoluna.x;
+       nodelunaLuz.x = -10000;
+        break;
+      case 'Full Moon':
+        print('Full Moo');
+        estadoluna.x = estadoluna.x - 300;
+        break;
+      case 'First Quarter':
+        print('First Quarter');
+        estadoluna.x = estadoluna.x - 80;
+        break;
+      case 'Third Quarter':
+        print('Third Quarter');
+        estadoluna.x = estadoluna.x + 80;
+        break;
+      default:
+    }
+
     DateTime date = DateTime.now();
     double hour = date.hour.toDouble();
 
@@ -71,18 +114,18 @@ class _MyHomePageState extends State<MyHomePage> with FlareController {
 
   bool advance(FlutterActorArtboard artboard, double elapsed) {
     _rockTime += elapsed * _speed;
-    
+
     //
     //_rock
     _rock.apply(_rockTime % _rock.duration, artboard, _rockAmount);
-    print(_cloudyTime);
+    
     if (_cloudyTime < 9.9 && end == false) {
       _cloudyTime += elapsed;
       _cloudy.apply(_cloudyTime % _cloudy.duration, artboard, 0.5);
     } else if (_cloudyTime > 0.1) {
       end = true;
-     
-     _cloudy.apply(_cloudyTime % _cloudy.duration, artboard, 0.5);
+
+      _cloudy.apply(_cloudyTime % _cloudy.duration, artboard, 0.5);
       _cloudyTime -= elapsed;
     }
 
